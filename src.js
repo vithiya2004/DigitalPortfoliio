7bafadfe5c68cc25967375dbf091ee2433f38b36
vithiya2004 <148960646+vithiya2004@users.noreply.github.com>
2023-10-25 15:10:51 +0530

    Add files via upload

---
+++ src.js
@@ -0,0 +1,18 @@
+const insta = () => {
+
+    window.open("https://www.instagram.com/anbu_throttler/" , '_blank');
+};
+
+const linkedin = () => {
+
+    window.open("https://www.linkedin.com/in/anbumani-v-1a86a7293",'_blank');
+};
+
+const twitter = () => {
+
+    window.open("https://twitter.com/anbu_throttler" , '_blank');
+};
+
+const resume = (resume) => {
+    window.open(resume, "_blank");
+}
